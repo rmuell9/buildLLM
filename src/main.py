ad71912e49af02f2bf12e59d1fa35a879f390403
rmuell9 <rmatthewmueller@gmail.com>
2025-11-12 14:19:54 -0600

    unk

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import re
+import tokenization
 
 with open("assets/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
@@ -17,38 +18,32 @@ vocab_size = len(all_words)
 print(f"Vocab size: {vocab_size}")
 
 
+# Seperate training texts and add token for unknown words
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+
 # Bijective mapping from tokens onto N^0 to create token IDs
-vocab = {token:integer for integer,token in enumerate(all_words)}
-# for i, item in enumerate(vocab.items()):
-#     print(item)
-#     if i >= 50:
-#         break
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+print(f"New vocab size: {len(vocab.items())}")
 
 
-class SimpleTokenizerV1:
-    def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i:s for s, i in vocab.items()}
+tokenizer = tokenization.SimpleTokenizerV1(vocab)
 
-    # input text -> token IDs
-    def encode(self, text):
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-
-    # token IDs -> input text
-    def decode(self, ids):
-        text = " ".join(self.int_to_str[i] for i in ids)
-
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
-        return text
-
-
-tokenizer = SimpleTokenizerV1(vocab)
 # Only works for words used in the-verdict
 text = """"It's the last he painted, you know,"
        Mrs. Gisburn said with pardonable pride."""
 ids = tokenizer.encode(text)
 print(ids)
 print(tokenizer.decode(ids))
+
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+# 1130: <|endoftext|>, 1131: <|unk|>
+tokenizer = tokenization.SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
