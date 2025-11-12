82040576b0f76ab144bdde7877064b0008a11d6c
rmuell9 <rmatthewmueller@gmail.com>
2025-11-12 17:58:44 -0600

    BPE

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
 import re
+import tiktoken
 import tokenization
 
 with open("assets/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-print(f"Word count: {len(raw_text)}")
+# print(f"Word count: {len(raw_text)}")
 
 
 # Tokenization - breaking down the training set into distinct words
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 
-print(f"Token count: {len(preprocessed)}")
+# print(f"Token count: {len(preprocessed)}")
 
 all_words = sorted(set(preprocessed))
 vocab_size = len(all_words)
-print(f"Vocab size: {vocab_size}")
+# print(f"Vocab size: {vocab_size}")
 
 
 # Seperate training texts and add token for unknown words
@@ -25,7 +26,7 @@ all_tokens.extend(["<|endoftext|>", "<|unk|>"])
 
 # Bijective mapping from tokens onto N^0 to create token IDs
 vocab = {token:integer for integer,token in enumerate(all_tokens)}
-print(f"New vocab size: {len(vocab.items())}")
+# print(f"New vocab size: {len(vocab.items())}")
 
 
 tokenizer = tokenization.SimpleTokenizerV1(vocab)
@@ -34,16 +35,28 @@ tokenizer = tokenization.SimpleTokenizerV1(vocab)
 text = """"It's the last he painted, you know,"
        Mrs. Gisburn said with pardonable pride."""
 ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
+# print(ids)
+# print(tokenizer.decode(ids))
 
 
 text1 = "Hello, do you like tea?"
 text2 = "In the sunlit terraces of the palace."
 text = " <|endoftext|> ".join((text1, text2))
-print(text)
+# print(text)
 
 # 1130: <|endoftext|>, 1131: <|unk|>
 tokenizer = tokenization.SimpleTokenizerV2(vocab)
-print(tokenizer.encode(text))
-print(tokenizer.decode(tokenizer.encode(text)))
+# print(tokenizer.encode(text))
+# print(tokenizer.decode(tokenizer.encode(text)))
+
+
+# Byte pair encoding
+tokenizer = tiktoken.get_encoding("gpt2")
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+     " of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+strings = tokenizer.decode(integers)
+print(strings)
